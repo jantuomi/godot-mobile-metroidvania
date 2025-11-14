bdfd9c7bca7e2003ea26aa2cee0c902a1a34622d
Jan Tuomi <jan@jantuomi.fi>
2025-11-14 12:16:04 +0200

    Focus menu button

--- level_main_menu.gd
+++ level_main_menu.gd
@@ -5,6 +5,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$TestLevelButton.grab_focus.call_deferred()
 	$TestLevelButton.pressed.connect(_test_level_button_pressed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
