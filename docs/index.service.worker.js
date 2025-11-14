d8c3cb5e5421b6daaad792ec4db5cb27e84a8a7d
Jan Tuomi <jan@jantuomi.fi>
2025-11-14 22:01:48 +0200

    Experiment with save files

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763149094|21932021128';
+const CACHE_VERSION = '1763150473|23312019331';
 /** @type {string} */
 const CACHE_PREFIX = 'MobileMetroidvan-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
--- root.gd
+++ root.gd
@@ -2,11 +2,20 @@ class_name Root
 extends Node2D
 
 @export var initial_level: PackedScene
+var save_state_path = "user://save_state.cfg"
+var save_state = ConfigFile.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var level = initial_level.instantiate()
 	add_child(level)
+	
+	# Testing
+	save_state.load(save_state_path)
+	print("loaded test_item: ", save_state.get_value("items", "test_item"))
+	save_state.set_value("items", "test_item", true)
+	save_state.save(save_state_path)
+	print(OS.get_data_dir())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -19,4 +28,6 @@ func change_level(level_name: String):
 	
 	var next_level = level.instantiate()
 	add_child(next_level)
-	
+
+func save():
+	save_state.save("user://save_state.cfg")
