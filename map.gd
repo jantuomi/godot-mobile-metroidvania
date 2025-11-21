31d7be95a48f3d36f2daac950dbc7cdaabeb5b86
Jan Tuomi <jan@jantuomi.fi>
2025-11-21 16:37:45 +0200

    Add full screen map and pause

--- map.gd
+++ map.gd
@@ -1,10 +1,14 @@
 class_name Map
 extends CanvasLayer
 
+@onready var top: Top = get_tree().root.get_node("Top")
 var active_map_room: MapRoom
 
 @export var inactive_color: Color
 @export var active_color: Color
+
+func _ready():
+	hide_map()
 
 func set_active_name(map_room_name: String):
 	if active_map_room:
@@ -17,3 +21,14 @@ func set_active_name(map_room_name: String):
 
 	if active_map_room:
 		active_map_room.color = active_color
+
+func connect_close_map(callable: Callable):
+	$UnpauseButton.connect("pressed", callable)
+
+func show_map():
+	visible = true
+	$UnpauseButton.mouse_filter = Control.MOUSE_FILTER_STOP
+	
+func hide_map():
+	visible = false
+	$UnpauseButton.mouse_filter = Control.MOUSE_FILTER_IGNORE
--- top.gd
+++ top.gd
@@ -2,11 +2,16 @@ class_name Top
 extends Node2D
 
 @export var initial_room: PackedScene
+
+var active_room_name: String
 var save_state_path = "user://save_state.cfg"
 var save_state = ConfigFile.new()
 
 func _ready() -> void:
+	$Map.hide_map()
+
 	var room = initial_room.instantiate()
+	active_room_name = room.name
 	room.name = "ActiveRoom"
 	room.process_mode = Node.PROCESS_MODE_PAUSABLE
 	add_child(room)
@@ -18,6 +23,10 @@ func _ready() -> void:
 	save_state.save(save_state_path)
 	print(OS.get_data_dir())
 
+func _process(_delta: float):
+	if Input.is_action_just_pressed("menu"):
+		on_esc_pressed()
+
 func save():
 	save_state.save("user://save_state.cfg")
 
@@ -26,6 +35,7 @@ func replace_active_room(new_room: Node, on_changed: Callable):
 	remove_child(current_room)
 	current_room.queue_free()
 	
+	active_room_name = new_room.name
 	new_room.name = "ActiveRoom"
 	new_room.connect("tree_entered", on_changed)
 	new_room.process_mode = Node.PROCESS_MODE_PAUSABLE
@@ -36,3 +46,18 @@ func get_map() -> Map:
 	
 func get_transition_manager() -> TransitionManager:
 	return $TransitionManager
+
+func on_esc_pressed():
+	if $Map.visible:
+		close_pause_menu()
+	elif active_room_name != "MainMenu":
+		open_pause_menu()
+
+func open_pause_menu():
+	get_tree().paused = true
+	$Map.show_map()
+	$Map.connect_close_map(close_pause_menu)
+
+func close_pause_menu():
+	get_tree().paused = false
+	$Map.hide_map()
