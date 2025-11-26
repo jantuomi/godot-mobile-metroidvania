adfd7c8a55112fa731b82417f27a18175c69e95e
Jan Tuomi <jan@jantuomi.fi>
2025-11-26 22:03:25 +0200

    Changes

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1764175235|10897656112';
+const CACHE_VERSION = '1764178526|14188520319';
 /** @type {string} */
 const CACHE_PREFIX = 'MobileMetroidvan-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
