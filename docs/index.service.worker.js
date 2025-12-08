c09d1026f21fd10623866dda8251fe144e358d53
Jan Tuomi <jan@jantuomi.fi>
2025-12-08 22:59:55 +0200

    Improve status page

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1764931733|33107827033';
+const CACHE_VERSION = '1765227407|25192476318';
 /** @type {string} */
 const CACHE_PREFIX = 'MobileMetroidvan-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
--- pause_menu_status.gd
+++ pause_menu_status.gd
@@ -1,2 +1,15 @@
 class_name PauseMenuStatus
 extends PauseMenuPage
+
+@onready var top: Top = get_tree().root.get_node("Top")
+@onready var inventory: Inventory = top.inventory
+
+const grey: Color = Color("4d4d4d")
+
+func _on_tree_entered() -> void:
+	if not is_node_ready(): return
+
+	if not inventory.hanging:
+		$AbilityHang.modulate = grey
+	if not inventory.zoop:
+		$AbilityZoop.modulate = grey
